23a9319ea729338fbb3cabfa11866046e7880b3c
lucianafigue <mlfluli@gmail.com>
2023-11-08 21:05:37 -0300

    modificaciones

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const app = express();
 const publicPath = path.resolve(__dirname,"./public"); //recibe la ruta donde se encuentra la carpeta de archivos publicos
 app.use( express.static(publicPath) );
 
-app.listen(3000,()=>{
-    console.log("Servidor corriendo en el puerto 3000");
-})
+// app.listen(3000,()=>{
+//     console.log("Servidor corriendo en el puerto 3000");
+// })
+
+require("dotenv").config(); //Hace que podamos usar una variable de entorno
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+console.log(`Servidor corriendo en el puerto ${port}`);
+});
 
 //para mostrar en el navegador
 
